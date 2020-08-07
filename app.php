1511c5f292b701cd4ec2bd2d32bf23d222550c58
puuzilook <69305041+puuzilook@users.noreply.github.com>
2020-08-07 21:45:53 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,1 +1,1 @@
-{ "app": [ { "url": "https://t.co/z4uaG7Thwm", "image": "https://i.imgur.com/7v34RNZ.png", "update": "yes" } ] }
+{ "app": [ { "url": "https://t.co/z4uaG7Thwm", "image": "https://i.imgur.com/7v34RNZ.png", "update": "no" } ] }
