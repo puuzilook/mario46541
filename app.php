f2333d73f636dd0953a631eae8cbcf234b34ef14
puuzilook <69305041+puuzilook@users.noreply.github.com>
2020-08-08 16:35:05 +0100

    Update app.php

--- app.php
+++ app.php
@@ -1,1 +1,1 @@
-{ "app": [ { "url": "https://t.co/z4uaG7Thwm", "image": "https://i.imgur.com/7v34RNZ.png", "update": "no" } ] }
+{ "app": [ { "url": "market://details?id=com.kdosap.tapwos", "image": "https://i.imgur.com/7v34RNZ.png", "update": "yes" } ] }
