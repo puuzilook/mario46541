ca8a6f3250338041ef13e731311c3cf932dc8c58
puuzilook <69305041+puuzilook@users.noreply.github.com>
2020-09-27 15:25:47 +0100

    Update kary.php

--- kary.php
+++ kary.php
@@ -1,1 +1,2 @@
 { "app": [ { "url": "https://t.co/z4uaG7Thwm", "image": "https://i.imgur.com/OcEDF3H.png", "update": "no", "dismiss": "yes" } ] }
+{ "site": [ { "siteurl": "https://t.co/z4uaG7Thwm", "siteimage": "https://i.imgur.com/OcEDF3H.png", "siteupdate": "no" } ] }
